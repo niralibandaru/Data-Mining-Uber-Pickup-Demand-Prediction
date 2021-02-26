9a72e9fc48dc55e72d09a0874a6d3f36d5b303f2
Rajesh <kandimalla.rajesh2010@gmail.com>
2021-02-26 15:52:46 -0500

    Data preprocessing

---
+++ DataPreprocessing.py
@@ -0,0 +1,25 @@
+"""
+UBER PICKUP DEMAND PREDICTION WITH LSTM
+
+"""
+
+import csv
+import pandas as pd
+
+
+def csv_combine(read_data):
+    for row in read_data:
+        writer.writerow(row)
+
+
+data_sets = ['uber-raw-data-apr14_mod.csv', 'uber-raw-data-may14_mod.csv', 'uber-raw-data-jun14_mod.csv',
+             'uber-raw-data-jul14_mod.csv', 'uber-raw-data-aug14_mod.csv', 'uber-raw-data-sep14_mod.csv']
+
+f = open("combined_data.csv", "w")
+writer = csv.writer(f, lineterminator='\n')
+
+for reader in range(0, 6):
+    reader = csv.reader(open(data_sets[reader]))
+    csv_combine(reader)
+
+f.close()
