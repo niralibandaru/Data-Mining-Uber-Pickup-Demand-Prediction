a926038f3209d5cf32dc6905e7cac62ae696e757
Rajesh <kandimalla.rajesh2010@gmail.com>
2021-02-26 15:57:08 -0500

    Data preprocessing

--- src/DataPreprocessing.py
+++ src/DataPreprocessing.py
@@ -6,14 +6,16 @@ UBER PICKUP DEMAND PREDICTION WITH LSTM
 import csv
 import pandas as pd
 
-
+#function to read the csv's and append it to the combined csv dataset file
 def csv_combine(read_data):
     for row in read_data:
         writer.writerow(row)
 
+#list of datasets available
 
 data_sets = ['uber-raw-data-apr14_mod.csv', 'uber-raw-data-may14_mod.csv', 'uber-raw-data-jun14_mod.csv',
              'uber-raw-data-jul14_mod.csv', 'uber-raw-data-aug14_mod.csv', 'uber-raw-data-sep14_mod.csv']
+
 
 f = open("combined_data.csv", "w")
 writer = csv.writer(f, lineterminator='\n')
